27d2e3f792eb4973b0035eb84a2f28d101b09216
P0159775 <xinhao.yang@pactera.com>
2020-07-26 12:47:12 +0800

    导航栏封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="home">
+  <div id="home">
+     <nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
     <h2>首页</h2>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import NavBar from '@/components/common/navbar/NavBar'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components:{
+    NavBar
+  }
 }
 </script>
 
+<style scoped>
+.nav-bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
+</style>
