8331730222370754ad405fca7a74013f6faeba73
P0159775 <xinhao.yang@pactera.com>
2020-09-15 17:25:36 +0800

    轮播图封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,33 +1,46 @@
 <template>
   <div id="home">
-     <nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
-    <h2>首页</h2>
+    <nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
+    <HomeSwiper :banners="banners"></HomeSwiper>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavBar from '@/components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
 import { getHomeMultidata } from '@/api/home.js'
  
 export default {
   name: 'Home',
   components:{
-    NavBar
+    NavBar,
+    HomeSwiper
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goodsList: {
+        'pop': {page: 1, list: []},
+        'new': {page: 1, list: []},
+        'sell': {page: 1, list: []}
+      },
+    }
   },
   created(){
     getHomeMultidata().then(res => {
       console.log('数据请求成功！')
-      this.results = res
       this.banners = res.data.banner.list
       this.recommends = res.data.recommend.list
-      console.log(res)
+      // console.log(res)
       console.log(res.data.banner.list)
-      console.log(res.data.recommend.list)
+      // console.log(res.data.recommend.list)
     })
   },
   data() {
     return {
+      data:null,
       banners: [],
       recommends: []
     }
@@ -36,13 +49,35 @@ export default {
 </script>
 
 <style scoped>
-#home{
-  width: 100%;
-}
+  #home {
+    /*position: relative;*/
+    height: 100vh;
+  }
 
-.nav-bar {
+  .nav-bar {
     background-color: var(--color-tint);
     font-weight: 700;
     color: #fff;
+  }
+
+  .content {
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+
+  .fixed {
+    position: fixed;
+    top: 44px;
+    left: 0;
+    right: 0;
+  }
+
+  .back-top {
+    position: fixed;
+    right: 10px;
+    bottom: 60px;
   }
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,39 @@
+<template>
+  <Swiper>
+    <SwiperItem v-for="item in banners" :key="item.acm">
+      <a :href="item.link">
+        <img :src="item.image" alt />
+      </a>
+    </SwiperItem>
+  </Swiper>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "@/components/common/swiper";
+export default {
+  name: "HomeSwiper",
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+  props: {
+    banners: {
+      type: Array,
+      default: [],
+    },
+  },
+  methods: {
+    stopTimer() {
+      this.$refs.swiper.stopTimer();
+    },
+    startTimer() {
+      if (this.$refs.swiper) {
+        this.$refs.swiper.startTimer();
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
