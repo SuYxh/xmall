68f2c2ec5351a4da1e265c1e81d027631b368421
P0159775 <xinhao.yang@pactera.com>
2020-09-16 10:00:45 +0800

    FeatureView封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,86 +1,97 @@
 <template>
   <div id="home">
-    <nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
+    <nav-bar class="nav-bar">
+      <div slot="center">购物街</div>
+    </nav-bar>
     <HomeSwiper :banners="banners"></HomeSwiper>
     <RecommendView :recommends="recommends"></RecommendView>
+    <FeatureView></FeatureView>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import NavBar from '@/components/common/navbar/NavBar'
-import HomeSwiper from './childComps/HomeSwiper'
-  import RecommendView from './childComps/RecommendView'
-import { getHomeMultidata } from '@/api/home.js'
- 
+import NavBar from "@/components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+import { getHomeMultidata } from "@/api/home.js";
+
 export default {
-  name: 'Home',
-  components:{
+  name: "Home",
+  components: {
     NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView,
   },
   data() {
     return {
       banners: [],
       recommends: [],
       goodsList: {
-        'pop': {page: 1, list: []},
-        'new': {page: 1, list: []},
-        'sell': {page: 1, list: []}
+        pop: { page: 1, list: [] },
+        new: { page: 1, list: [] },
+        sell: { page: 1, list: [] },
       },
-    }
+    };
   },
-  created(){
-    getHomeMultidata().then(res => {
-      console.log('数据请求成功！')
-      this.banners = res.data.banner.list
-      this.recommends = res.data.recommend.list
+  created() {
+    getHomeMultidata().then((res) => {
+      console.log("数据请求成功！");
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
       // console.log(res)
-      console.log(res.data.banner.list)
-      console.log(res.data.recommend.list)
-    })
+      console.log(res.data.banner.list);
+      console.log(res.data.recommend.list);
+    });
   },
   data() {
     return {
-      data:null,
+      data: null,
       banners: [],
-      recommends: []
-    }
+      recommends: [],
+    };
   },
-}
+};
 </script>
 
 <style scoped>
-  #home {
-    /*position: relative;*/
-    height: 100vh;
-  }
+#home {
+  /*position: relative;*/
+  height: 100vh;
+  /* padding-top: 44px; */
+}
 
-  .nav-bar {
-    background-color: var(--color-tint);
-    font-weight: 700;
-    color: #fff;
-  }
+.nav-bar {
+  background-color: var(--color-tint);
+  font-weight: 700;
+  color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 99;
+}
 
-  .content {
-    position: absolute;
-    top: 44px;
-    bottom: 49px;
-    left: 0;
-    right: 0;
-  }
+.content {
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
 
-  .fixed {
-    position: fixed;
-    top: 44px;
-    left: 0;
-    right: 0;
-  }
+.fixed {
+  position: fixed;
+  top: 44px;
+  left: 0;
+  right: 0;
+}
 
-  .back-top {
-    position: fixed;
-    right: 10px;
-    bottom: 60px;
-  }
+.back-top {
+  position: fixed;
+  right: 10px;
+  bottom: 60px;
+}
 </style>
